1e6e79d0d4f52af8c28366bb69fbaa4a265065ac
Nataniel Santos <nathan.santos159@hotmail.com>
2021-12-29 17:11:14 -0300

    Updates

---
+++ precache-manifest.69345193d90eb5c35532f945d7394877.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3eed942ace8bbde2bc91e95d7c47f04",
+    "url": "/trivia-redux/index.html"
+  },
+  {
+    "revision": "024cc420e206275842fa",
+    "url": "/trivia-redux/static/css/main.a1e5219c.chunk.css"
+  },
+  {
+    "revision": "ac47bdea0cee5be5c353",
+    "url": "/trivia-redux/static/js/2.57b3887d.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trivia-redux/static/js/2.57b3887d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024cc420e206275842fa",
+    "url": "/trivia-redux/static/js/main.511d148e.chunk.js"
+  },
+  {
+    "revision": "0e23cf9fc2d3827658f7",
+    "url": "/trivia-redux/static/js/runtime-main.b6a8bd90.js"
+  },
+  {
+    "revision": "b5adcc53952b20bd1171e4df966f42af",
+    "url": "/trivia-redux/static/media/sliders-h-solid (1).b5adcc53.svg"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-redux/static/media/trivia.466d153e.png"
+  }
+]);
