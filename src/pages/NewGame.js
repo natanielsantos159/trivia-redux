66f05bddaed44fdefe458eaad3c0714ea3f142ff
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-14 20:03:20 -0300

    Cria forms do New Game

--- src/pages/NewGame.js
+++ src/pages/NewGame.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Card from '../components/Card';
 import logo from '../images/trivia.png';
+import { categories } from '../data';
 
 export default class NewGame extends Component {
   render() {
@@ -10,9 +12,41 @@ export default class NewGame extends Component {
         </section>
 
         <section className="content-container">
-          <section className="new-game-section">
-            <h1>Novo jogo</h1>
-          </section>
+          <Card title="Novo jogo">
+            <label htmlFor="question-amount">
+              Numero de questões:
+              <select id="question-amount" name="question-amount">
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+              </select>
+            </label>
+            <label htmlFor="difficulty">
+              Dificuldade:
+              <select id="difficulty" name="difficulty">
+                <option value="easy">Fácil</option>
+                <option value="medium">Intermediário</option>
+                <option value="hard">Difícil</option>
+              </select>
+            </label>
+            <label htmlFor="category-select">
+              Categoria:
+              <select name="category-select" id="category-select">
+                <option value="any">Qualquer Categoria</option>
+                { categories.map(({ id, name }) => <option value={ id } key={ id }>{name}</option>)}
+              </select>
+            </label>
+            <span>Tipo:</span>
+            <label htmlFor="multiple-choice">
+              Múltipla Escolha
+              <input id="multiple-choice" type="radio" value="multiple" name="question-type" checked />
+            </label>
+            <label htmlFor="true-false">
+              Verdadeiro ou Falso
+              <input id="true-false" type="radio" value="boolean" name="question-type" />
+            </label>
+
+          </Card>
         </section>
       </main>
     );
