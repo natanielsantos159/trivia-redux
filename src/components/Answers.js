bc052bd29015fc94ed7a79b1742dc97165a72bf6
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-14 17:05:04 -0300

    Organiza css do componente Answers

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import '../styles/Answers.css';
 
 export default class Answers extends Component {
   encodeUtf8(string) {
