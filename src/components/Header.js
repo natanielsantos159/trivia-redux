9409a26eacebb29789ed6efe7522ba68fac53d85
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-16 21:57:36 -0300

    Altera tamanho da pontuação no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -67,7 +67,7 @@ class Header extends Component {
           <span className="player-score">
             Pontos:
             {' '}
-            <span data-testid="header-score">{ score }</span>
+            <span data-testid="header-score" className="header-score">{ score }</span>
           </span>
         </header>
       </section>
